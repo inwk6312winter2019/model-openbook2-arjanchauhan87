93323ae0c4774647a4912aedc05ed74e0d56ace7
arjan <A.chauhan@dal.ca>
2019-02-06 21:28:16 +0000

    first

--- task2.py
+++ task2.py
@@ -1,9 +1,9 @@
 def tuple_required(filename ):
 	fin = open(filename,"r")
 	for line in fin:
-		line = line.split(" ")
+		line = line.split(",")
 		tp = (line[2], line[4], line[6], line[7])
 		print(tp)
 
-fin = open("street_centerlines.csv","r")
-print(tuple_required("street_centrelines.csv"))
+fin = open("Street_Centrelines.csv","r")
+print(tuple_required("Street_Centrelines.csv"))
